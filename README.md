c3bdb2a827c5336af96bae5cf5cc1af5ae8cc08d
mohclips <nick@kiwi-hacker.net>
2025-01-05 22:25:40 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Grafana docker image
 
 needs the data volume replacing as local storage to access the data config files
+
+
+# Air Sensor
+
+IoT device SDS011 -> PHP script on ubuntu server in ./html/ folder
+
+http://172.30.5.67/air-sensor/data.php
+
+
--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@
 
 export DOMAINNAME=homelan.local
 # otherwise is tails the logs
-docker-compose pull && docker-compose up --detach --build --force-recreate --no-deps
+#### NO ####docker compose pull && docker compose up --detach --build --force-recreate --no-deps
